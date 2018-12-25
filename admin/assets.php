92de8f731a644e9257bf2e0cf0818271513890c3
Taylor Gorman <me@taylorpatrickgorman.com>
2018-12-25 11:32:16 -0600

    🛠 cleanup and assets

---
+++ admin/assets.php
@@ -0,0 +1,12 @@
+<?php
+
+use Launchpad\Setup;
+
+add_action( 'admin_enqueue_scripts', function(){
+
+	$v = WP_DEBUG ? time() : Setup\version();
+
+	//wp_enqueue_style( Setup\name(), plugin_dir_url( __FILE__ ). 'css/styles.css', [], $v );
+	//wp_enqueue_script( Setup\name(), plugin_dir_url( __FILE__ ). 'js/scripts.js', [], $v, true );
+
+} );
--- launchpad.php
+++ launchpad.php
@@ -15,9 +15,8 @@
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) die;
 
-/*
-** Load this plugin first, so its resources are available to everyone.
-*/
+// Load this plugin first, so its resources are available to everyone.
+//
 add_action( 'activated_plugin', function(){
 
 	$plugin_url = plugin_basename( __FILE__ );
@@ -33,9 +32,8 @@ add_action( 'activated_plugin', function(){
 
 } );
 
-/**
-	* Activation & Deactivation
-	*/
+// Activation & Deactivation
+//
 //register_activation_hook( __FILE__, function() {
 //
 //	require 'admin/activation.php';
@@ -49,35 +47,18 @@ add_action( 'activated_plugin', function(){
 //
 //} );
 
-/*
-** Constants
-		TO DO: Get rid of these
-*/
-define( 'LP_IS_LOCAL', $_SERVER['SERVER_NAME'] == 'localhost' );
-define( 'LP_PATH', plugin_dir_path(__FILE__) );
-define( 'LP_URL', plugin_dir_url(__FILE__) );
-
-/*
-** Settings
-		TO DO: Make this a function
-*/
-global $lp;
-$lp = get_option('launchpad');
-
-/**
-	* Global
-	*/
+// Global
+//
 require 'lib/setup.php';
 
-/**
-	* Back-end
-	*/
+// Back-end
+//
+require_once 'admin/assets.php';
 require_once 'admin/admin-fields.php';
 require_once 'admin/settings-launchpad.php';
 require_once 'admin/settings-contact.php';
 require_once 'admin/roles.php';
 require_once 'admin/page.php';
-require_once 'admin/enqueues.php';
 require_once 'admin/menu.php';
 require_once 'admin/tinymce.php';
 require_once 'admin/dashboard-widgets.php';
@@ -85,34 +66,28 @@ require_once 'admin/media.php';
 require_once 'admin/users.php';
 require_once 'admin/post-types-sorting.php';
 require_once 'admin/layouts.php';
-
 //require_once 'admin/new-user-email.php';
 //require_once 'admin/featured-icon.php';
 
-require_once 'widgets/section-navigation.php';
-require_once 'widgets/jp-related-posts.php';
+// Front-end
+//
+require_once 'public/lp-register-post-type.php';
+require_once 'public/lp-register-taxonomy.php';
+require_once 'public/get-post-thumbnail-url.php';
+require_once 'public/minutes-to-read.php';
+require_once 'public/date-range.php';
+require_once 'public/get-adjacent-page.php';
+require_once 'public/is-any-tax.php';
+require_once 'public/get-jetpack-related-posts.php';
+require_once 'public/the-field-markup.php';
+require_once 'public/meta.php';
+require_once 'public/wp-head.php';
+require_once 'public/scripts.php';
+require_once 'public/excerpt.php';
+require_once 'public/content.php';
+require_once 'public/classes.php';
+require_once 'public/theme-support.php';
+require_once 'public/images.php';
 
-/**
-	* Front-end
-	*/
-require_once 'theme/lp-register-post-type.php';
-require_once 'theme/lp-register-taxonomy.php';
-require_once 'theme/get-post-thumbnail-url.php';
-require_once 'theme/minutes-to-read.php';
-require_once 'theme/date-range.php';
-require_once 'theme/get-adjacent-page.php';
-require_once 'theme/is-any-tax.php';
-require_once 'theme/get-jetpack-related-posts.php';
-require_once 'theme/the-field-markup.php';
-
-require_once 'theme/meta.php';
-require_once 'theme/wp-head.php';
-require_once 'theme/scripts.php';
-require_once 'theme/excerpt.php';
-require_once 'theme/content.php';
-require_once 'theme/classes.php';
-require_once 'theme/theme-support.php';
-require_once 'theme/images.php';
-
-if ( ! empty( $lp['post-formats'] ) )
-	require_once 'theme/format-meta.php';
+//if ( ! empty( $lp['post-formats'] ) )
+//	require_once 'public/format-meta.php';
