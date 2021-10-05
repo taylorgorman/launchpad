34b2421ab904bbe1ee520fb6d8cf6415a4c8b48a
Taylor Gorman <me@taylorpatrickgorman.com>
2021-10-04 19:25:18 -0500

    code format

--- admin/assets.php
+++ admin/assets.php
@@ -4,9 +4,20 @@ use Launchpad\Setup;
 
 add_action( 'admin_enqueue_scripts', function(){
 
-	$v = WP_DEBUG ? time() : Setup\version();
+	$version = WP_DEBUG ? time() : Setup\version();
 
-	wp_enqueue_style( Setup\name(), plugin_dir_url( __FILE__ ) . 'css/styles.css', [], $v );
-	//wp_enqueue_script( Setup\name(), plugin_dir_url( __FILE__ ). 'js/scripts.js', [], $v, true );
+	wp_enqueue_style(
+    Setup\name(),
+    plugin_dir_url( __FILE__ ) . 'css/styles.css',
+    [],
+    $version
+  );
+	// wp_enqueue_script(
+  //   Setup\name(),
+  //   plugin_dir_url( __FILE__ ). 'js/scripts.js',
+  //   [],
+  //   $version,
+  //   true
+  // );
 
 } );
