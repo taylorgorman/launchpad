d5061459968597857c5d32e3c215bb212607a5b3
Taylor Gorman <me@taylorpatrickgorman.com>
2022-06-26 20:29:08 -0500

    remove settings_error code
    can always look up again with the @see link at the top of the file

--- admin/settings.php
+++ admin/settings.php
@@ -23,18 +23,6 @@ add_action( 'admin_menu', function () {
         return;
       }
 
-      /*
-      // check if the user have submitted the settings
-      // WordPress will add the "settings-updated" $_GET parameter to the url
-      if ( isset( $_GET['settings-updated'] ) ) {
-        // add settings saved message with the class of "updated"
-        add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved hm', 'wporg' ), 'updated' );
-      }
-
-      // show error/update messages
-      settings_errors( 'wporg_messages' );
-      */
-
       ?>
       <div class="wrap">
         <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
