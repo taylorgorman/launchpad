9c3e93691fe4a43fe6daacb2bece1ab3941dac25
Taylor Gorman <me@taylorpatrickgorman.com>
2022-06-30 11:49:34 -0500

    clean up

--- setup/constants.php
+++ setup/constants.php
@@ -1,22 +1,7 @@
 <?php
-// All the shit we use in the whole plugin
 
 namespace Launchpad\Setup;
 
 const TITLE = 'Launchpad';
-const NAME = 'launchpad'; // Constants must be straight values
+const NAME = 'launchpad'; // Constants can't be variables
 const VERSION = '2.0.0';
-
-// /**
-//  * The one options value (array) for/from the settings page
-//  */
-// function option( $name ) {
-
-// 	$options = get_option( name() );
-
-// 	if ( empty( $options[$name] ) )
-// 		return null;
-// 	else
-// 		return $options[ $name ];
-
-// }
