532ac07d5f6b2fd5729f98f9c725b780ccce1b9d
Taylor Gorman <me@taylorpatrickgorman.com>
2022-06-30 16:47:41 -0500

    documentation

--- changes/groups/content.php
+++ changes/groups/content.php
@@ -7,10 +7,13 @@ function content() {
     'title' => 'Content',
     'changes' => [
       [
-        'title' => 'Set shortened excerpt length to 25 characters',
+        'title' => 'Set shortened excerpt length to 25 words (default is 55)',
         'name' => 'excerpt-length',
         'execute' => function () {
 
+          /**
+           * @see https://developer.wordpress.org/reference/hooks/excerpt_length/
+           */
           add_filter( 'excerpt_length', function () { return 25; } );
 
         },
