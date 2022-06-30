cf36954f87fa8b6852fd61e24fa3a4dcfaa6ac18
Taylor Gorman <me@taylorpatrickgorman.com>
2022-06-30 14:28:41 -0500

    add page excerpt

--- changes/groups/pages.php
+++ changes/groups/pages.php
@@ -10,6 +10,11 @@ function pages() {
         'title' => 'Add excerpt to pages',
         'name' => 'page-excerpt',
         'execute' => function () {
+
+          add_action( 'init', function(){
+            add_post_type_support( 'page', 'excerpt' );
+          } );
+          
         },
       ],
     ],
