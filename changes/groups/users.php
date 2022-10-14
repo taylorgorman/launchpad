998d43dc5acdc80da5addec93ce2ffd8f91c1cba
Taylor Gorman <me@taylorpatrickgorman.com>
2022-10-14 18:01:45 -0500

    settings to add social user profile fields

--- changes/groups/users.php
+++ changes/groups/users.php
@@ -7,27 +7,55 @@ function users() {
     'title' => 'Users',
     'changes' => [
       [
-        'title' => UNSUPPORTED . 'Add Instagram username field',
+        'title' => 'Add Instagram username field',
         'name' => 'user-field-instagram',
         'execute' => function () {
+
+          add_filter( 'user_contactmethods', function( $fields ) {
+            return array_merge( $fields, [
+              'instagram' => 'Instagram username'
+            ] );
+          } );
+
         },
       ],
       [
-        'title' => UNSUPPORTED . 'Add Twitter username field',
+        'title' => 'Add Twitter username field',
         'name' => 'user-field-twitter',
         'execute' => function () {
+
+          add_filter( 'user_contactmethods', function( $fields ) {
+            return array_merge( $fields, [
+              'twitter' => 'Twitter username'
+            ] );
+          } );
+
         },
       ],
       [
-        'title' => UNSUPPORTED . 'Add LinkedIn URL field',
+        'title' => 'Add LinkedIn URL field',
         'name' => 'user-field-linkedin',
         'execute' => function () {
+
+          add_filter( 'user_contactmethods', function( $fields ) {
+            return array_merge( $fields, [
+              'linkedin' => 'LinkedIn URL'
+            ] );
+          } );
+
         },
       ],
       [
-        'title' => UNSUPPORTED . 'Add Facebook URL field',
+        'title' => 'Add Facebook URL field',
         'name' => 'user-field-facebook',
         'execute' => function () {
+
+          add_filter( 'user_contactmethods', function( $fields ) {
+            return array_merge( $fields, [
+              'facebook' => 'Facebook URL'
+            ] );
+          } );
+
         },
       ],
     ],
