5565a297d07914f93e35cfbd2dd4018a4e7fb525
Taylor Gorman <taylor@thegorman.group>
2023-04-29 19:36:21 -0500

    scrape over everything left that might matter

--- changes/groups/media.php
+++ changes/groups/media.php
@@ -11,6 +11,7 @@ function media() {
         'title' => UNSUPPORTED . 'Add Open Graph image size',
         'name' => 'open-graph-image-size',
         'execute' => function () {
+          // 1200 x 630 (still?)
         },
       ],
       [
@@ -71,12 +72,20 @@ function media() {
 }
 
 function get_all_image_sizes() {
-  $default_image_sizes = get_intermediate_image_sizes();
-  $image_sizes = [];
-  foreach ( $default_image_sizes as $size ) {
-    $image_sizes[ $size ]['width'] = intval( get_option( "{$size}_size_w" ) );
-    $image_sizes[ $size ]['height'] = intval( get_option( "{$size}_size_h" ) );
-    $image_sizes[ $size ]['crop'] = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
-  }
-  return $image_sizes;
+	global $_wp_additional_image_sizes;
+
+	$image_sizes         = [];
+	$default_image_sizes = get_intermediate_image_sizes();
+
+	foreach ( $default_image_sizes as $size ) {
+		$image_sizes[ $size ]['width']  = intval( get_option( "{$size}_size_w" ) );
+		$image_sizes[ $size ]['height'] = intval( get_option( "{$size}_size_h" ) );
+		$image_sizes[ $size ]['crop']   = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
+	}
+
+	if ( isset( $_wp_additional_image_sizes ) && count( $_wp_additional_image_sizes ) ) {
+		$image_sizes = array_merge( $image_sizes, $_wp_additional_image_sizes );
+	}
+
+	return $image_sizes;
 }
